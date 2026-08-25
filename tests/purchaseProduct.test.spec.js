5694217bdf39b0241ffc76705d666041ea3ed9fc
Harsh <harshsunami75@gmail.com>
2026-08-25 18:05:33 +0530

    pushed wrong code

--- tests/purchaseProduct.test.spec.js
+++ tests/purchaseProduct.test.spec.js
@@ -93,6 +93,9 @@ describe("Product purchase testing", () => {
         await menuPage.openMenu();
         //click on logout button
         await menuPage.clickLogout()
+        funcitosjdhfjd(){
+            djfiodsjf
+        }
 
        
         await loginPage.verifyLoginPage();
